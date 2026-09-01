91ec90a78491bd2d005d1f0cc3c19ed0e13f44c4
JonBSkii <jbrown934@boyslatin.net>
2026-09-01 14:27:31 +0000

    datatypes notes

---
+++ datatypes.py
@@ -0,0 +1,15 @@
+# STRING-characters that are wrapped within quotes.
+# characters CAN BE LETTERS, NUMBERS, AND SYMBOLS.
+# can use single or double quotation mark
+"Jonathan Brown"
+
+"123 main street"
+
+"this cost $100 dollars."
+
+# The hashtag is a marker that makes anything written behind it invisible to the computer
+# The word syntax is a word that's used to describe written words
+# the core 4 data types are (strings) (integers) (floats)(booleans)
+# Integers- datatypes that are just whole numbers, or numbers without decimal point.
+# Float- A number with a decimal.
+# Booleans- Its either true or false.   
